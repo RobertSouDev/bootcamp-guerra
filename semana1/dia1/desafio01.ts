4d651c6a2092dbe67172ffaaee3379cc976df3dd
RobertSouDev <rogerhallks1@gmail.com>
2025-05-17 19:10:42 -0300

    feat implementar verificação de números primo com otimização e explicação

--- semana1/dia1/desafio01.ts
+++ semana1/dia1/desafio01.ts
@@ -6,15 +6,31 @@
 //}
 
 function isPrime(numero: number): boolean {
-    if(numero < 2){
-        return true
-    }
-    if( numero  % 2 === 0) {
-        if( numero % 3 === 0){
-            return false
-        }
-    }
-    return true
-}
+    if(numero < 2) return false
+     for(let i = 2 ; i< numero ;i++){
+        if (numero % i === 0) return false
+     }
 
+     return true
+}
+console.log(isPrime(1))
+console.log(isPrime(2))
+console.log(isPrime(9))
+console.log(isPrime(6))
 console.log(isPrime(11))
+
+
+// ou melhor ainda ficaria assim
+function isPrime2(numero: number): boolean {
+    if (numero < 2) return false;
+  
+    for (let i = 2; i <= Math.sqrt(numero); i++) {
+      if (numero % i === 0) return false;
+    }
+  
+    return true;
+  }
+  
+
+
+
