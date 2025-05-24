61c8eeff03e232a78c0d295081a5a55cae85f435
RobertSouDev <rogerhallks1@gmail.com>
2025-05-24 00:34:13 -0300

    feat: adiciona anotações de comandos git

--- semana1/dia1/reverse-stringHard.ts
+++ semana1/dia1/reverse-stringHard.ts
@@ -1,14 +1,13 @@
 function isPalindrome22(x: number): boolean {
-    let inverted: number = 0
-    const y = x
-    while( x > 0) {
-      const lastNumber = x % 10
-      inverted = (inverted * 10 ) + lastNumber
-      x = ~~( x/ 10)
+    let inverted: number = 0;
+    const y = x;
+    while (x > 0) {
+        let last = x % 10;
+        inverted = inverted * 10 + last;
+        x = ~~(x / 10);
     }
-  
-    return y == inverted
-  
-  };
 
-  console.log(isPalindrome22(121))
+    return y === inverted;
+}
+
+console.log(isPalindrome22(-121));
